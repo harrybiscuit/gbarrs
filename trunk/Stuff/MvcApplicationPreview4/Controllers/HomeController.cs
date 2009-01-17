239efd0272a42b35088be38ec3577499a486bb0e
gbarrs@gmail.com <gbarrs@gmail.com@702933fa-e34d-11dd-b00f-9fabb4b2ff13>
2009-01-17 10:26:56 +0000

    Add Stuff

---
+++ trunk/Stuff/MvcApplicationPreview4/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplicationPreview4.Models;
+
+namespace MvcApplicationPreview4.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewData["Title"] = "Home Page";
+            ViewData["Message"] = "Welcome to ASP.NET MVC!";
+            ViewData["Colour"] = Colour.red;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewData["Title"] = "About Page";
+            ViewData["Colour"] = Colour.blue;
+
+            return View();
+        }
+    }
+}
