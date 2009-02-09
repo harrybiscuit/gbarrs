1f0100bedb83a6312687741f668e6befe426fd1c
gbarrs <gbarrs@702933fa-e34d-11dd-b00f-9fabb4b2ff13>
2009-02-09 20:53:15 +0000

    Added cassini stuff and database stuff

---
+++ Stuff/CassiniAndWatin/BriteBIT.Test/CassiniWrapper/WebServerConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using BriteBIT.Stuff.Validation.Castle;
+using BriteBIT.Validation;
+using Castle.Components.Validator;
+
+namespace BriteBIT.Stuff.CassiniWrapper
+{
+    public class WebServerConfiguration
+    {
+        [ValidateFilePath("The Cassisni executable could not be found at the specified path.")]
+        [ValidateNonEmpty("The path to the Cassisni executable is missing.")]
+        public string CassiniExecutableFilePath { get; set; }
+
+        [ValidateNonEmpty("The file name of the default Web site page is missing.")]
+        public string DefaultWebPageFileName { get; set; }
+
+        public bool OpenDefaultPageAutomatically { get; set; }
+
+        /// <summary>
+        /// Specifying a port number of 0 (zero) with cause the
+        /// Web server to start with an auto-assignedIp address
+        /// </summary>
+        [ValidateInteger("The port number that the server instance should run on is invalid.")]
+        public int PortNumber { get; set; }
+
+        [ValidateDirectory("The specified Web site root directory could not be found.")]
+        [ValidateNonEmpty("The name of the Web site root directory is missing.")]
+        public string WebSiteRootFolder { get; set; }
+    }
+}
---
+++ Stuff/CassiniAndWatin/BriteBIT.Test/Validation/Castle/ValidateFilePathAttribute.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Castle.Components.Validator;
+
+namespace BriteBIT.Validation
+{
+    /// <summary>
+    /// Validate that the specifed file exists.
+    /// </summary>
+    [Serializable]
+    public class ValidateFilePathAttribute : AbstractValidationAttribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ValidateFilePathAttribute"/> class.
+        /// </summary>
+        public ValidateFilePathAttribute()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ValidateFilePathAttribute"/> class.
+        /// </summary>
+        /// <param name="errorMessage">The error message.</param>
+        public ValidateFilePathAttribute(String errorMessage) : base(errorMessage)
+        {
+        }
+
+        /// <summary>
+        /// Constructs and configures an <see cref="IValidator"/>
+        /// instance based on the properties set on the attribute instance.
+        /// </summary>
+        /// <returns></returns>
+        public override IValidator Build()
+        {
+            IValidator validator = new FilePathValidator();
+
+            ConfigureValidatorMessage(validator);
+
+            return validator;
+        }
+    }
+}
