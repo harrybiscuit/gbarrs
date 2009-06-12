8249cf383e751133c587f99f92df38a28d6fb68f
gbarrs@gmail.com <gbarrs@gmail.com@702933fa-e34d-11dd-b00f-9fabb4b2ff13>
2009-06-12 12:12:26 +0000

    work in progress

---
+++ trunk/Stuff/CassiniAndWatin/BriteBIT.Test/Gac.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.EnterpriseServices.Internal;
+using System.IO;
+using System.Reflection;
+
+namespace BriteBIT.Stuff
+{
+    /// <summary>
+    /// This class abstracts GAC installation and removal.
+    /// </summary>
+    public sealed class Gac
+    {
+        public static void InstallAssembly(string assemblyPath)
+        {
+            string errorMessage;
+            if (String.IsNullOrEmpty(assemblyPath))
+            {
+                throw new FileNotFoundException("Could not find assembly file to add to the GAC.", assemblyPath);
+            }
+            
+            try
+            {
+                Assembly assembly = Assembly.ReflectionOnlyLoadFrom(assemblyPath);
+                byte[] pkey = assembly.GetName().GetPublicKeyToken();
+                if (pkey.Length == 0)
+                {
+                    errorMessage = string.Format("Cannot install an unsigned assembly into the GAC. '{0}'", assemblyPath);
+                    throw new NotSupportedException(errorMessage);
+                }
+            }
+            catch (BadImageFormatException ex)
+            {
+                errorMessage = string.Format("The assembly is not a CLR assembly. '{0}'", assemblyPath);
+                throw new BadImageFormatException(errorMessage, ex);
+            }
+            catch (FileLoadException ex)
+            {
+                if (!ex.Message.Contains("has already loaded from a different location"))
+                {
+                    errorMessage = string.Format("CLR assembly found but could not be loaded. '{0}'", assemblyPath);
+                    throw new FileLoadException(errorMessage, ex);
+                }
+            }
+
+            var publish = new Publish();
+            publish.GacInstall(assemblyPath.Trim());
+            
+        }
+
+        public static void RemoveAssembly(string assemblyPath)
+        {
+            var publish = new Publish();
+            try
+            {
+                publish.GacRemove(assemblyPath.Trim());
+            }
+            catch (FileNotFoundException)
+            {
+                
+                // Do nothing.  If the assembly is not there
+                // then we expect this to happen.
+            }
+            
+        }
+
+        public static bool IsInGac(string assemblyFullName)
+        {
+            try
+            {
+                return Assembly.ReflectionOnlyLoad(assemblyFullName) != null && Assembly.ReflectionOnlyLoad(assemblyFullName).GlobalAssemblyCache;
+            }
+            catch (FileNotFoundException)
+            {
+                return false;            
+            }
+        }
+    }
+}
---
+++ trunk/Stuff/CassiniAndWatin/BriteBIT.Test/Validation/Castle/FilePathValidator.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using Castle.Components.Validator;
+
+namespace BriteBIT.Validation
+{
+    public class FilePathValidator : AbstractValidator
+    {
+        /// <summary>
+        /// Gets a value indicating whether this validator supports browser validation.
+        /// </summary>
+        /// <value>
+        /// <see langword="true" /> if browser validation is supported; otherwise, <see langword="false" />.
+        /// </value>
+        public override bool SupportsBrowserValidation
+        {
+            get { return false; }
+        }
+
+        /// <summary>
+        /// Check that the directory specified by this property exists
+        /// </summary>
+        /// <param name="instance"></param>
+        /// <param name="fieldValue"></param>
+        /// <returns><c>true</c> if the field is OK</returns>
+        public override bool IsValid(object instance, object fieldValue)
+        {
+            var path = fieldValue as string;
+            return !string.IsNullOrEmpty(path) && File.Exists(path);
+        }
+    }
+}
